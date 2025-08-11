7f1ae29e91539c3f4b43ece5ad4df8f7c37a8e0e
miniluca <theminiluca@gmail.com>
2025-08-11 20:04:41 +0900

    오탈자 수정:

--- src/main/java/com/gmail/theminiluca/grim/guardian/UpdateChecker.java
+++ src/main/java/com/gmail/theminiluca/grim/guardian/UpdateChecker.java
@@ -2,27 +2,33 @@ package com.gmail.theminiluca.grim.guardian;
 
 import org.bukkit.Bukkit;
 
-import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
 public class UpdateChecker {
-    private final int resourseId;
+    private final int resourceId;
 
-    public UpdateChecker(int resourseId) {
-        this.resourseId = resourseId;
+    public UpdateChecker(int resourceId) {
+        this.resourceId = resourceId;
     }
 
     public void getLastVersion(Consumer<String> consumer) {
         Bukkit.getScheduler().runTaskAsynchronously(GrimGuardian.getInstance(), () -> {
-            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourseId).openStream()) {
-                Scanner scanner = new Scanner(inputStream);
-                if (scanner.hasNext()) {
-                    consumer.accept(scanner.next());
+
+            try {
+                URI uri = new URI("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId);
+                URL url = uri.toURL();
+
+                try (InputStream inputStream = url.openStream();
+                     Scanner scanner = new Scanner(inputStream)) {
+                    if (scanner.hasNext()) {
+                        consumer.accept(scanner.next());
+                    }
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
