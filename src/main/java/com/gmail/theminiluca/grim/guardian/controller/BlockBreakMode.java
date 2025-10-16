c8212d73c4be3b372f4829de5889d10ec7321a48
miniluca <theminiluca@gmail.com>
2025-10-16 23:21:35 +0900

    BlockBreakMode 진행 변수 및 float 계산 방식 리팩터링

--- src/main/java/com/gmail/theminiluca/grim/guardian/controller/BlockBreakMode.java
+++ src/main/java/com/gmail/theminiluca/grim/guardian/controller/BlockBreakMode.java
@@ -10,7 +10,6 @@ import ac.grim.grimac.utils.inventory.EnchantmentHelper;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.item.ItemStack;
-import com.github.retrooper.packetevents.util.Vector3i;
 import com.gmail.theminiluca.grim.guardian.GrimGuardian;
 import com.gmail.theminiluca.grim.guardian.event.BlockImpactEvent;
 import com.gmail.theminiluca.grim.guardian.hook.ServerLevel;
@@ -20,17 +19,12 @@ import com.gmail.theminiluca.grim.guardian.utils.config.model.formula.Formula;
 import com.google.common.base.Preconditions;
 import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -41,21 +35,23 @@ import static com.gmail.theminiluca.grim.guardian.listener.BukkitListener.BLOCK_
 @Getter
 public class BlockBreakMode {
 
-    private final @NotNull ServerPlayer serverPlayer;
-    private final @NotNull ServerLevel serverLevel;
-    private final @NotNull Player player;
-    private final @NotNull GrimPlayer grimPlayer;
-    private final @NotNull PlayerInteractEvent event;
-    private final @NotNull Block block;
-    private final @NotNull BlockBreakSpeed blockBreakSpeed;
-    private final @NotNull org.bukkit.inventory.ItemStack itemStack;
-    private final @NotNull ItemStack packetItemStack;
-    private final int maxBuildHeight;
+    protected final @NotNull ServerPlayer serverPlayer;
+    protected final @NotNull ServerLevel serverLevel;
+    protected final @NotNull Player player;
+    protected final @NotNull GrimPlayer grimPlayer;
+    protected final @NotNull PlayerInteractEvent event;
+    protected final @NotNull Block block;
+    protected final @NotNull BlockBreakSpeed blockBreakSpeed;
+    protected final @NotNull org.bukkit.inventory.ItemStack itemStack;
+    protected final @NotNull ItemStack packetItemStack;
+    protected final int maxBuildHeight;
+    protected final float blockHardness;
 
 
-    private float workingTime = 0;
-    private int backProgress = 0;
-    private @Nullable BukkitTask bukkitTask;
+    protected float totalProgress = 0;
+    protected float tickProgress = 0;
+    protected byte backProgress = 0;
+    protected @Nullable BukkitTask bukkitTask;
 
     public BlockBreakMode(@NotNull ServerPlayer serverPlayer, @NotNull ServerLevel serverLevel, @NotNull PlayerInteractEvent event) {
         this.serverPlayer = serverPlayer;
@@ -71,6 +67,7 @@ public class BlockBreakMode {
         this.grimPlayer = Objects.requireNonNull(GrimAPI.INSTANCE.getPlayerDataManager()
                 .getPlayer(player.getUniqueId()), "grimPlayer cannot be null");
         this.maxBuildHeight = serverLevel.getMaxBuildHeight();
+        this.blockHardness = getBlockHardness();
         
     }
 
@@ -83,7 +80,7 @@ public class BlockBreakMode {
     public void destroyBlockProgress(final int progress) {
         serverLevel.destroyBlockProgress(serverPlayer.getPlayer().getEntityId() << 5, block, progress);
     }
-    private void cancelTask() {
+    protected void cancelTask() {
         if (bukkitTask == null) return;
         serverLevel.cancelBlockProgress(player.getEntityId() << 5, block);
         bukkitTask.cancel();
@@ -97,38 +94,97 @@ public class BlockBreakMode {
 
     }
 
-    private boolean checkBreakConditions() {
-        if (!blockBreakSpeed.isBreakable()) {
-            BlockBreakMode.this.cancel();
+    protected final boolean checkBreakConditions() {
+        if (isUnbreakable()) {
+            cancel();
             return true;
         }
-        if (!player.isOnline() || !grimPlayer.platformPlayer.isOnline()) {
-            BlockBreakMode.this.cancel();
+        if (isPlayerOffline()) {
+            cancel();
             return true;
         }
-        if (player.getInventory().getItemInMainHand() == itemStack) {
-            BlockBreakMode.this.cancel();
+        if (isItemChanged()) {
+            cancel();
             return true;
         }
-        if (block.getType().isAir()) {
-            BlockBreakMode.this.cancel();
+        if (isBlockGone()) {
+            cancel();
             return true;
         }
         return false;
     }
+
+    protected boolean isUnbreakable() {
+        return !blockBreakSpeed.isBreakable();
+    }
+
+    protected boolean isPlayerOffline() {
+        return !player.isOnline() || !grimPlayer.platformPlayer.isOnline();
+    }
+
+    protected boolean isItemChanged() {
+        return player.getInventory().getItemInMainHand() == itemStack;
+    }
+
+    protected boolean isBlockGone() {
+        return block.getType().isAir();
+    }
+
+    protected float getBlockHardness() {
+        return block.getType().getHardness();
+    }
+
+    protected byte getProgress() {
+        return (byte) ((totalProgress / ((((float) blockBreakSpeed.getTick()) + 6.0F))) * 10);
+    }
+
+    protected float multiplyHaste() {
+        return (float) ConfigYaml.getInstance().getFormula(Formula.HASTE).evaluate(grimPlayer, blockHardness);
+    }
+
+    protected float multiplyMiningFatigue() {
+        return (float) ConfigYaml.getInstance().getFormula(Formula.MINING_FATIGUE).evaluate(grimPlayer, blockHardness);
+    }
+
+    protected float multiplyOffGround() {
+        return 0.2f;
+    }
+
+    protected float multiplyAttribute() {
+        return (float) grimPlayer.compensatedEntities.self.getAttributeValue(Attributes.BLOCK_BREAK_SPEED);
+    }
+
+    protected float multiplyExtra() {
+        return 1.0f;
+    }
+
+    protected float multiplyAquaAffinity() {
+        return 0.2f;
+    }
+
+    protected float defaultWorkTime() {
+        return 1.0f;
+    }
+    protected float requiredProgress() {
+        return 10.0f;
+    }
+
+    protected void setTotalProgress() {
+        totalProgress += Math.max(tickProgress, 0);
+    }
+
     public void run() {
         cancel();
         bukkitTask = new BukkitRunnable() {
 
             @Override
             public void run() {
-                float hardness = block.getType().getHardness();
                 if (checkBreakConditions()) {
                     BlockBreakMode.this.cancel();
                     return;
                 }
 
-                byte progress = (byte) ((workingTime / ((((float) blockBreakSpeed.getTick()) + 6.0F))) * 10);
+                byte progress = getProgress();
                 BlockImpactEvent blockImpactEvent = new BlockImpactEvent(player, block, BlockFace.valueOf(event.getBlockFace().name()), player.getInventory().getItemInMainHand(), blockBreakSpeed.isInstantBreak());
                 blockImpactEvent.callEvent();
                 if (!blockImpactEvent.isCancelled())
@@ -137,30 +193,29 @@ public class BlockBreakMode {
                         destroyBlockProgress(progress);
                     }
 
-                float worktime = 1.0F;
-//                                int hasteLevel = Math.max(digSpeed.isEmpty() ? 0 : digSpeed.getAsInt(), conduit.isEmpty() ? 0 : conduit.getAsInt());
-//                            worktime *= (float) ConfigHandler.evaluate(ConfigHandler.HASTE_EQUATION, grimPlayer, block.getType().getHardness());
-                worktime *= (float) ConfigYaml.getInstance().getFormula(Formula.HASTE).evaluate(grimPlayer, hardness);
-                worktime *= (float) ConfigYaml.getInstance().getFormula(Formula.MINING_FATIGUE).evaluate(grimPlayer, hardness);
+                tickProgress = defaultWorkTime();
+                tickProgress *= multiplyHaste();
+                tickProgress *= multiplyMiningFatigue();
                 if (!grimPlayer.packetStateData.packetPlayerOnGround) {
-                    worktime /= 5.0F;
+                    tickProgress *= multiplyOffGround();
                 }
                 if (grimPlayer.fluidOnEyes == FluidTag.WATER) {
                     if (grimPlayer.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_21)
                             && PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_21)) {
-                        worktime *= (float) grimPlayer.compensatedEntities.self.getAttributeValue(Attributes.BLOCK_BREAK_SPEED);
+                        tickProgress *= multiplyAttribute();
                     } else {
                         if (EnchantmentHelper.getMaximumEnchantLevel(grimPlayer.getInventory(), EnchantmentTypes.AQUA_AFFINITY, ac.grim.grimac.shaded.com.github.retrooper.packetevents.PacketEvents
                                 .getAPI().getServerManager().getVersion().toClientVersion()) == 0) {
-                            worktime /= 5.0F;
+                            tickProgress *= multiplyAquaAffinity();
                         }
                     }
                 }
-                if (!blockImpactEvent.isCancelled()) workingTime += Math.max(worktime, 0);
+                tickProgress *= multiplyExtra();
+                if (!blockImpactEvent.isCancelled()) setTotalProgress();
                 backProgress = progress;
 
-                if ((progress >= 10 || blockImpactEvent.isInstantBreak()) && !blockImpactEvent.isCancelled()) {
-                    if (!serverPlayer.canInteractWithBlock(block, 0.0D)) {
+                if ((progress >= requiredProgress() || blockImpactEvent.isInstantBreak()) && !blockImpactEvent.isCancelled()) {
+                    if (!serverPlayer.canInteractWithBlock(block, 1.0D)) {
                         return;
                     }
                     BlockBreakMode.this.cancel();
